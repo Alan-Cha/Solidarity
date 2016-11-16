a3b1f4b097253df959087feacb43c86e3f58612f
Alan Cha <ac3805@columbia.edu>
2016-11-15 23:04:01 -0500

    Added  screenshots

---
+++ Mobile/Solidarity/app/src/main/java/com/amazonaws/models/nosql/PanicMessagesDO.java
@@ -0,0 +1,57 @@
+package com.amazonaws.models.nosql;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBIndexRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@DynamoDBTable(tableName = "solidarity-mobilehub-1621301408-panicMessages")
+
+public class PanicMessagesDO {
+    private String _userId;
+    private String _dtime;
+    private String _address;
+    private Double _groupId;
+
+    @DynamoDBHashKey(attributeName = "userId")
+    @DynamoDBAttribute(attributeName = "userId")
+    public String getUserId() {
+        return _userId;
+    }
+
+    public void setUserId(final String _userId) {
+        this._userId = _userId;
+    }
+    @DynamoDBRangeKey(attributeName = "dtime")
+    @DynamoDBAttribute(attributeName = "dtime")
+    public String getDtime() {
+        return _dtime;
+    }
+
+    public void setDtime(final String _dtime) {
+        this._dtime = _dtime;
+    }
+    @DynamoDBAttribute(attributeName = "address")
+    public String getAddress() {
+        return _address;
+    }
+
+    public void setAddress(final String _address) {
+        this._address = _address;
+    }
+    @DynamoDBAttribute(attributeName = "groupId")
+    public Double getGroupId() {
+        return _groupId;
+    }
+
+    public void setGroupId(final Double _groupId) {
+        this._groupId = _groupId;
+    }
+
+}
